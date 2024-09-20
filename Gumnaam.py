68bfc90337f377dc2aa759e4c961f2b84c87595c
1322Gumnaam <sukhdevsin.g.h.7.3.4.7.3@gmail.com>
2024-09-20 22:39:02 +0530

    Gumnaam.py

--- Gumnaam.py
+++ Gumnaam.py
@@ -16,7 +16,7 @@ bot = telebot.TeleBot('7537334155:AAH-ZKG1JXupeUoJtxsSeLhNnI-kNcbeQqI')
 from pymongo import MongoClient
 import certifi
 
-MONGO_URI = 'mongodb+srv://GumnaamHuni:Goru123@@gumnaamhuni.wsvoh.mongodb.net'
+MONGO_URI = 'mongodb+srv://GumnaamHuni:Goru123@@gumnaamhuni.wsvoh.mongodb.net/?retryWrites=true&w=majority&appName=GumnaamHuni'
 
 try:
     client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
